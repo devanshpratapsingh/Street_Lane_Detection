9f8de3309239dfd2c2af61d4d24fcd51b21456b2
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-12-05 20:42:24 +0530

    Add files via upload

---
+++ OpenCV_Starting.py
@@ -0,0 +1,13 @@
+import matplotlib.pylab as plt
+import cv2
+import numpy as np
+
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    #channel_count = img.shape[2]
+    match_mask_color = 255
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
